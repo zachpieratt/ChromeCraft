7ceffe2be489615c44bb9a3e06f68faf1ff9c1d8
Zach Pieratt <zachpieratt@gmail.com>
2024-11-19 09:20:09 -0500

    small fix

--- src/attribution.tsx
+++ src/attribution.tsx
@@ -5,7 +5,7 @@ let currentYear: string = new Date().getFullYear().toString();
 const Attribution = () => {
     return (
         <div className="footer">
-            <img className="ghLogo" alt="gitHub" src="https://ik.imagekit.io/chromecraft/tr:h-18,w-18/github.png" />
+            <img height={18} width={18} className="ghLogo" alt="gitHub" src="https://ik.imagekit.io/chromecraft/tr:h-18,w-18/github.png" />
             <br />
             <span >{currentYear}</span>
             <span> Zach Pieratt</span>
